40fc0554fc7ddba0daad2784d5b62e5fe4df0c6c
cgladis <cgladis@student.21-school.ru>
2022-04-06 22:42:17 +0300

    chapter 2

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+import flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return '<h1>Hello World!</h1>'
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
